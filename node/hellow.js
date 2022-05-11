594d4dc1ee66a739630fe5d3355091f4f452d8a8
Fabíola Diniz <fabioladiniz36@hotmail.com>
2022-05-11 16:53:38 -0300

    Primeira atividade

---
+++ node/hellow.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Hellow!');
+})
+
+server.listen(port, () => console.log(`server started on port ${port};`+'press Ctr -C to Vanessa do Barro'))
