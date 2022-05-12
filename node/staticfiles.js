8c56d50c1d3d71a60b0df163939eadcc042b4e6b
Rebehk <rebehk.jordao@gmail.com>
2022-05-11 21:17:37 -0300

    Update staticfiles.js

--- node/staticfiles.js
+++ node/staticfiles.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req,res) => {
  //normaliza a url  removendo a querystring e a barra final
  //opcional e usando letras minusculas
- const path = req.url.replace(/\/?(?:\?.*)?$/, ''). toLowwerCase()
+ const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowwerCase()
  switch(path) {
      case '':
          res.writeHead(200,{'Content-Type':'text/plain'})
